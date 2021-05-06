599b9f1c0605df64ccda57c6e0570353a2931e19
Ahmed Ali <sucounix@gmail.com>
2021-05-06 13:39:47 +0200

    commet

--- upload_docker.sh
+++ upload_docker.sh
@@ -11,8 +11,8 @@ dockerpath=public.ecr.aws/v2o3x6u2/capstone
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/v2o3x6u2
-docker build -t capstone .
-docker tag capstone:latest public.ecr.aws/v2o3x6u2/capstone:latest
+docker build -t capstone:v1 .
+docker tag capstone:v1 public.ecr.aws/v2o3x6u2/capstone:v1
 # Step 3:
 # Push image to a docker repository
-docker push public.ecr.aws/v2o3x6u2/capstone
+docker push public.ecr.aws/v2o3x6u2/capstone:v1
